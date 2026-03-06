1810b8c2c702202c7c0b52730ac669c7b658b5d0
Arnstein Kleven <arnsteinkleven@gmail.com>
2026-03-06 11:06:01 +0100

    Update to fix build

--- build.rs
+++ build.rs
@@ -8,13 +8,18 @@ fn main() {
 	println!("cargo:rustc-link-search=/usr/lib");
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    let sysroot = format!("--sysroot={}", env::var("LIBCLANG_PATH").unwrap());
-    let incdir = format!("-I{}", env::var("VIDEODEV_PATH").unwrap());
+    let incdir = format!("-I{}", env::var("VIDEODEV_PATH").unwrap_or_else(|_| "/usr/include".to_string()));
 
-    let bindings = bindgen::Builder::default()
+    let mut builder = bindgen::Builder::default()
         .header("wrapper.h")
-		.clang_arg(sysroot)
-		.clang_arg(incdir)
+        .clang_arg(incdir);
+
+    // V4L2_SYSROOT is used for cross-compilation (e.g. Yocto). Not needed for native builds.
+    if let Ok(sysroot) = env::var("V4L2_SYSROOT") {
+        builder = builder.clang_arg(format!("--sysroot={}", sysroot));
+    }
+
+    let bindings = builder
         .generate()
         .expect("Failed to generate bindings");
 
